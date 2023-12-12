c7feae85299e7c5ae1fc37bf158c46cf4c32c09f
김혜송(Hyesong Kim)/커머스플랫폼개발팀/SKP <hyesong.kim@sk.com>
2023-12-13 02:10:03 +0900

    feat: add keymap field listener to configure with pressing keymap

--- src/main/kotlin/com/github/lauvsong/languagecursor/settings/AppSettingsComponent.kt
+++ src/main/kotlin/com/github/lauvsong/languagecursor/settings/AppSettingsComponent.kt
@@ -3,6 +3,8 @@ package com.github.lauvsong.languagecursor.settings
 import com.intellij.ui.ColorPanel
 import com.intellij.ui.components.JBLabel
 import com.intellij.util.ui.FormBuilder
+import java.awt.event.KeyAdapter
+import java.awt.event.KeyEvent
 import javax.swing.JPanel
 import javax.swing.JTextField
 
@@ -14,6 +16,13 @@ class AppSettingsComponent {
             .addComponentFillVertically(JPanel(), 0)
             .panel
     }
-    val keymap: JTextField = JTextField()
+    val keymap: JTextField = JTextField().apply {
+        // Display the key in the text field every time the user enters a key
+        addKeyListener(object : KeyAdapter() {
+            override fun keyTyped(e: KeyEvent) {
+                text = KeyEvent.getKeyText(e.keyCode)
+            }
+        })
+    }
     val cursorColor: ColorPanel = ColorPanel()
 }
