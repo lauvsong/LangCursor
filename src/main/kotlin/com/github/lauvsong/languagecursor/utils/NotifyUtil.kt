14fb8a27cbc6a52254b37c271761aa833f70bea5
lauvsong <hyesong.kim@sk.com>
2024-01-01 23:42:16 +0900

    feat: add notification util

---
+++ src/main/kotlin/com/github/lauvsong/languagecursor/utils/NotifyUtil.kt
@@ -0,0 +1,15 @@
+package com.github.lauvsong.languagecursor.utils
+
+import com.intellij.notification.NotificationGroupManager
+import com.intellij.notification.NotificationType
+import com.intellij.openapi.project.Project
+
+object NotifyUtil {
+
+    fun byBalloonWarning(project: Project, content: String) {
+        NotificationGroupManager.getInstance()
+            .getNotificationGroup("balloon")
+            .createNotification(content, NotificationType.WARNING)
+            .notify(project)
+    }
+}
