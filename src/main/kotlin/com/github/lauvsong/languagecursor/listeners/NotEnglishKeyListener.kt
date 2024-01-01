5837d191834042f85b9ec10e1b1bb6aa9dae2699
김혜송(Hyesong Kim)/커머스플랫폼개발팀/SKP <hyesong.kim@sk.com>
2024-01-01 17:13:58 +0900

    feat: add detect english listener

---
+++ src/main/kotlin/com/github/lauvsong/languagecursor/listeners/NotEnglishKeyListener.kt
@@ -0,0 +1,71 @@
+package com.github.lauvsong.languagecursor.listeners
+
+import com.github.lauvsong.languagecursor.settings.AppSettingsState
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.colors.EditorColors
+import com.intellij.openapi.editor.colors.EditorColorsManager
+import com.intellij.openapi.editor.event.CaretEvent
+import com.intellij.openapi.editor.event.CaretListener
+import com.intellij.ui.JBColor
+import java.awt.Color
+import java.awt.im.InputContext
+import java.util.Locale
+
+class NotEnglishKeyListener : CaretListener {
+
+    private val originalCursorColor: Color = EditorColorsManager.getInstance()
+        .globalScheme
+        .getColor(EditorColors.CARET_COLOR)
+        ?: JBColor.BLACK
+
+    override fun caretPositionChanged(event: CaretEvent) {
+        val editor = event.editor
+        updateCursorColor(editor)
+    }
+
+    private fun updateCursorColor(editor: Editor) {
+        val settings = AppSettingsState.instance
+        val isEnglishInput = isEnglishInput()
+        val cursorColor = if (!isEnglishInput) settings.cursorColor else editor.colorsScheme.defaultForeground
+
+        if (isEnglishInput) {
+            restoreOriginalCursorColor(editor)
+        } else {
+            noEnglishCursorColor(editor, cursorColor)
+        }
+    }
+
+    private fun isEnglishInput() : Boolean {
+        val locale = InputContext.getInstance().locale
+        val language = locale.language
+        val country = locale.country
+
+        // Explanation for not solely relying on constant `Locale.ENGLISH`:
+        // In some Locale configurations, the language code might not be explicitly provided.
+        // e.g., "_US_UserDefined_252"
+        // Therefore, Locales with country codes are also considered as English input.
+        // Canada (CA) is not included here because both English and French are used in Canada.
+        // ** If you encounter any edge cases, please feel free to open an issue on GitHub. **
+        if (language == Locale.ENGLISH.language) {
+            return true
+        }
+
+        if (country == Locale.US.country
+            || country == Locale.UK.country) {
+            return true
+        }
+
+        return false
+    }
+
+    private fun noEnglishCursorColor(editor: Editor, newColor: Color) {
+        val scheme = editor.colorsScheme
+        scheme.setColor(EditorColors.CARET_COLOR, newColor)
+    }
+
+    private fun restoreOriginalCursorColor(editor: Editor) {
+        val scheme = editor.colorsScheme
+        scheme.setColor(EditorColors.CARET_COLOR, originalCursorColor)
+    }
+
+}
